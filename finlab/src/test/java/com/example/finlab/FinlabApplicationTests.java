851f445a91ba8b236bc4699de885531d8e30d265
Ishaan Yadav <ishaanyad08@gmail.com>
2025-02-15 18:13:59 +0530

    Adding finlab directory

---
+++ finlab/src/test/java/com/example/finlab/FinlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
